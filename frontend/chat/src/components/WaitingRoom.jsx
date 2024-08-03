3f0e8fee8a56eaf5a09633f04909e6793c847b74
ALEX-PC\Alex <alex@altrec.ru>
2024-08-03 18:52:55 +0500

    Довёл функционал до работоспособности, использовал для бэка ASP.NET Core, SignalR, в качестве кэша Redis, для фронта React

--- frontend/chat/src/components/WaitingRoom.jsx
+++ frontend/chat/src/components/WaitingRoom.jsx
@@ -1,18 +1,38 @@
-import {Button, Heading, Text} from "@chakra-ui/react";
+import { Button, Heading, Input, Text } from "@chakra-ui/react";
+import { useState } from "react";
 
-export const WaitingRoom = () => {
+export const WaitingRoom = ({ joinChat }) => {
+    const [userName, setUserName] = useState();
+    const [chatRoom, setChatRoom] = useState();
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        joinChat(userName, chatRoom);
+    };
+
     return (
-        <form className="max-w-sm w-full bg-white p-8 rounded shadow-lg">
+        <form onSubmit={onSubmit} className="max-w-sm w-full bg-white p-8 rounded shadow-lg">
             <Heading>Онлайн чат</Heading>
             <div className="mb-4">
-                <Text fontSize={"small"}>Имя пользователя</Text>
-                <input name="userName" placeholder="Введите ваше имя"/>
+                <Text fontSize={"small"}>Имя пользователя:</Text>
+                <Input
+                    onChange={(e) => setUserName(e.target.value)}
+                    className="p-2"
+                    name="userName"
+                    placeholder="Введите ваше имя"
+                />
             </div>
-            <div className="mb-4">
-                <Text fontSize={"small"}>Название чата</Text>
-                <input name="chatRoom" placeholder="Введите название чата"/>
+                        <div className="mb-4">
+                <Text fontSize={"small"}>Название чата:</Text>
+                <Input
+                    onChange={(e) => setChatRoom(e.target.value)}
+                    className="p-2"
+                    name="chatRoom"
+                    placeholder="Введите название чата"
+                />
             </div>
-            <Button title="submit" colorScheme="blue">Присоединиться</Button>
+            <Button type="submit" colorScheme="blue">Присоединиться</Button>
         </form>
     );
 }
+
